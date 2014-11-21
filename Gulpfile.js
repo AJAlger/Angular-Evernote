1ec9a6e52d545791f8c2fb1990ea5b842d55c6c0
Abdullah Alger <abdullahalger@me.com>
2014-11-21 21:43:17 +0200

    Added more Gulp and Bourbon/neat

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,19 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+		sass = require('gulp-sass'),
+		autoprefixer = require('gulp-autoprefixer'),
+		minifycss = require('gulp-minify-css'),
+		rename = require('gulp-rename'),
+		neat = require('node-neat').includePaths;
+
+
+var paths = {
+
+		sass: 'app/stylesheets/sass/',
+		css: 'app/stylesheets/css/',
+		script: 'app/scripts/'
+
+};
+
 
 gulp.task('express', function() {
 	var express = require('express');
@@ -7,6 +22,20 @@ gulp.task('express', function() {
 	app.listen(8080);
 });
 
-gulp.task('default', ['express'], function() {
+
+// Gulp Task to SASS - Bourbon and Neat are working
+gulp.task('styles', function() {
+	return gulp.src(paths.sass + '*.scss')
+		.pipe(sass({
+			includePaths: ['styles'].concat(neat)
+		}))
+	.pipe(gulp.dest(paths.css))
+});
+
+gulp.task('scripts', function() {
+	return gulp.src(paths.script)
+});
+
+gulp.task('default', ['express', 'styles'], function() {
 
 });
