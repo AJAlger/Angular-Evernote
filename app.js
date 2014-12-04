8c83a9ad39b73f22f31f768dc426566408d2c0e4
Abdullah Alger <abdullahalger@me.com>
2014-12-04 13:57:38 +0200

    Angular-Material Themes Added

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var express = require('express'),
 // =================================================================//
 app.set('port', process.env.PORT || 9001); // Set to 9001 to not interfere with Gulp 9000
 app.use(serveStatic('app', {'index': false})); // Where the files and gets login.html first
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride());
